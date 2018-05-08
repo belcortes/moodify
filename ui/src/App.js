749037e7fc368728b85b2d1faa2f9fe8bb6abe93
belcortes <belcortes@gmail.com>
2018-05-08 11:07:26 -0700

    removing comments in App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,37 +3,12 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import { fetchPlaylists } from './actions/index'
-// import axios from 'axios'
-// import queryString from 'query-string';
-// import SpotifyWebApi from 'spotify-web-api-js'
-
-// const spotifyApi = new SpotifyWebApi()
 
 import PlaylistList from './containers/PlaylistList'
 
 class App extends Component {
 
   componentDidMount() {
-    // let parsed = queryString.parse(window.location.search);
-    // let accessToken = parsed.access_token;
-    // if (!accessToken)
-    //   return;
-    // axios.get('https://api.spotify.com/v1/me/playlists', {
-    //   headers: {'Authorization': 'Bearer ' + accessToken}
-    // })
-    // .then(response => {
-    //   console.log(response.data)
-    //   this.setState({playlists: response.data.items})
-    // })
-    // .catch((error) => {
-    //   console.log('Error retrieving!')
-    //   console.log(error)
-    // })
-
-    // spotifyApi.setAccessToken(accessToken);
-    // spotifyApi.getMe(function(data) {
-    //   console.log(data)
-    // })
     this.props.fetchPlaylists()
   }
 
